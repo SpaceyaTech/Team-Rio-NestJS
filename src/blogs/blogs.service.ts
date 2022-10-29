f90a9598ace713c220ab0d2aecadb28085004a59
Ernest Wambua <ernestwambua2@gmail.com>
2022-10-29 14:26:29 +0300

    Implemented blog service

--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BlogPost } from './blog.entity';
 
 @Injectable()
-export class BlogsService {}
+export class BlogsService {
+  constructor(@InjectRepository(BlogPost) private repo: Repository<BlogPost>) {}
+
+  async findById(id: string) {
+    const blog = await this.repo.findOneBy({ id });
+    if (!blog) throw new NotFoundException('Blog not found');
+    return blog;
+  }
+
+  find() {
+    return this.repo.find();
+  }
+
+  async update(id: string, attrs: Partial<BlogPost>) {
+    const blog = await this.findById(id);
+    Object.assign(blog, attrs);
+    return this.repo.save(blog);
+  }
+
+  delete(id: string) {
+    return this.repo.delete({ id });
+  }
+}
