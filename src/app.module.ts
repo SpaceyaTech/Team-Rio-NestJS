eb7f0f804521711e91dbd6c060483c51f4205c17
Ernest Wambua <ernestwambua2@gmail.com>
2022-10-29 08:50:48 +0300

    Created users and blog modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogsModule } from './blogs/blogs.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [BlogsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
