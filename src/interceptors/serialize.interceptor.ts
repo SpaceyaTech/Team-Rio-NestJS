81af787525f2d6d7d47df5b17e4c9a55c5fb4aac
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-03 08:18:44 +0300

    Implemented the serialize interceptor

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+export interface ClassInterface {
+  new (...args: any[]): {};
+}
+
+export const Serialize = (dto: ClassInterface) => {
+  return UseInterceptors(new SerializeInterceptor(dto));
+};
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: ClassInterface) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          strategy: 'excludeAll',
+          excludeExtraneousValues: true,
+          enableImplicitConversion: true,
+        });
+      }),
+    );
+  }
+}
