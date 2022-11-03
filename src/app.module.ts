5d499fb4396a501727b73b4d44ca69d843a598ec
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-03 07:00:47 +0300

    Setup passport authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
     }),
     BlogsModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as passport from 'passport';
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
-  }
+  if (process.env.NODE_ENV === 'production') app.use(helmet());
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: 'hjsajk',
+      resave: false,
+      saveUninitialized: true,
+      // cookie: { secure: true } // preferable when using https
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   await app.listen(5000);
 }
