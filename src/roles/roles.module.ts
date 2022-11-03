bceedbd50b6cd5403b8d5d5090f43cfde7fd9b0d
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-03 11:33:58 +0300

    Fixed a bug in the roles module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './role.entity';
 
 @Module({
   exports: [RolesService],
+  imports: [TypeOrmModule.forFeature([Role])],
   providers: [RolesService],
   controllers: [RolesController],
 })
