5ef4382f2482383671dc8581ad5504c6ca7ada5b
Ernest Wambua <ernestwambua2@gmail.com>
2022-11-28 19:54:21 +0300

    Created the categories controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,71 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { CategoriesService } from './categories.service';
+import { CategoryDto } from './dtos/category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private service: CategoriesService) {}
+
+  @ApiOperation({
+    summary: 'Get categories',
+    description: 'Fetch a list of all categories',
+  })
+  @Get()
+  getCategories() {
+    return this.service.find();
+  }
+
+  @ApiOperation({
+    summary: 'Get blogs in a category',
+    description: 'Fetch all blogs within a certain category',
+  })
+  @Get('blogs')
+  getBlogsByCategory(@Param('category') category: string) {
+    return this.service.findBlogsByCategory(category);
+  }
+
+  @ApiOperation({
+    summary: 'Get a category',
+    description: 'Fetch a single category',
+  })
+  @Get(':name')
+  getCategory(@Param('name') name: string) {
+    return this.service.findOne(name);
+  }
+
+  @ApiOperation({
+    summary: 'Create a category',
+    description: 'Create a new category',
+  })
+  @Post()
+  createCategoy(@Body() body: CategoryDto) {
+    return this.service.create(body);
+  }
+
+  @ApiOperation({
+    summary: 'Edit a category',
+    description: 'Edit the name of a category',
+  })
+  @Patch(':name')
+  editCategory(@Param('name') name: string, @Body() body: CategoryDto) {
+    return this.service.update(name, body);
+  }
+
+  @ApiOperation({
+    summary: 'Delete a category',
+    description: 'Delete a category by name',
+  })
+  @Delete(':name')
+  deleteCategory(@Param('name') name: string) {
+    return this.service.delete(name);
+  }
+}
