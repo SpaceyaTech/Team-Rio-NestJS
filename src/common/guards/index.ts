82169a873634cc01897ebbce2fc0868ff2b8c81c
Ernest Wambua <ernestwambua2@gmail.com>
2022-12-05 15:26:36 +0300

    Created the common module

--- src/common/guards/index.ts
+++ src/common/guards/index.ts
@@ -2,3 +2,4 @@ export * from './at.guard';
 export * from './rt.guard';
 export * from './roles.guard';
 export * from './is-owner.guard';
+export * from './require-auth.guard';
--- src/common/index.ts
+++ src/common/index.ts
@@ -1,1 +1,5 @@
 export * from './filters';
+export * from './interceptors';
+export * from './guards';
+export * from './dtos';
+export * from './decorators';
